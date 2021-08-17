a571bd883fcf35f84a9d145037a6137fc954066c
aditibhatt32 <67057405+aditibhatt32@users.noreply.github.com>
2021-08-17 21:11:57 +0530

    Oops-Assigning

---
+++ oops-Assigning.py
@@ -0,0 +1,11 @@
+class Employee:
+ ID=None
+ Salary=None
+ department=None
+Steve=Employee()
+Steve.ID=37
+Steve.Salary=2500
+Steve.department="human resource"
+print(Steve.ID)
+print(Steve.Salary)
+print(Steve.department)
