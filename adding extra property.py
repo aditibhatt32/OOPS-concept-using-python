c00d037a3e96a07b171aaf20a988be6bc9ddcdf5
aditibhatt32 <67057405+aditibhatt32@users.noreply.github.com>
2021-08-18 23:54:22 +0530

    Oops- Adding extra properties

---
+++ adding extra property.py
@@ -0,0 +1,13 @@
+class Employee:
+ ID=None
+ Salary=None
+ department=None
+Steve=Employee()
+Steve.ID=37
+Steve.Salary=2500
+Steve.department="human resource"
+Steve.title="Manager"
+print(Steve.ID)
+print(Steve.Salary)
+print(Steve.department)
+print(Steve.title)
