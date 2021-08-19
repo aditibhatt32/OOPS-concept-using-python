e3e427bacfed7c5bee301c1a3de412bf33750634
aditibhatt32 <67057405+aditibhatt32@users.noreply.github.com>
2021-08-19 23:16:50 +0530

    oops-Adding initializer method

---
+++ Initializer.py
@@ -0,0 +1,10 @@
+class Employee:
+    def __init__(self,ID,salary,department):
+     self.ID=ID
+     self.salary=salary
+     self.department=department
+steve=Employee(3789,2500,"HR")
+print("ID",steve.ID)
+print("salary",steve.salary)
+print("Department",steve.department)
+    
