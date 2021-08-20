79396adfa84168db14a77d9f847ad255249d06d5
aditibhatt32 <67057405+aditibhatt32@users.noreply.github.com>
2021-08-20 23:08:52 +0530

    OOPS-use of class variable

---
+++ class variable.py
@@ -0,0 +1,10 @@
+class Player:
+ teamName='Liverpool'
+ def __init__(self,name):
+    self.name=name
+p1=Player('Mark')
+p2=Player('Steve')
+print("Name:",p1.name)
+print("TeamName:",p1.teamName)
+print("Name:",p2.name)
+print("TeamName:",p2.teamName)
