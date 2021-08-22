905d4da9c378e55b9f8bfef84fc66b335b2c60f6
aditibhatt32 <67057405+aditibhatt32@users.noreply.github.com>
2021-08-22 22:36:19 +0530

    OOPS-Class Method

---
+++ class method.py
@@ -0,0 +1,8 @@
+class Player:
+    teamName= 'Liverpool'
+    def __init__(self,name):
+        self.name=name
+    @classmethod
+    def getTeamName(cls):
+        return cls.teamName
+print(Player.getTeamName())
